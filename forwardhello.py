51a94c9c86b872568a3a93f3a4f90a4dd370d4b0
CJS <christian.stromberger65@stu.austin.isd.org>
2021-07-13 11:42:26 -0700

    Update forwardhello.py

--- forwardhello.py
+++ forwardhello.py
@@ -37,7 +37,7 @@ class GoForward():
 	    # as long as you haven't ctrl + c keeping doing...
         while not rospy.is_shutdown():
             for i in range(100):
-                speeds = self.fixwheelspeed(0.5, .50001)
+                speeds = self.fixwheelspeed(0.5, .2)
                 move_cmd.linear.x = speeds[0]
                 move_cmd.angular.z = speeds[1]
 	            # publish the velocity
