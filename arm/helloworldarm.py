f86363404d70624da7b462b528d92029f59ad10a
CJS <christian.stromberger65@stu.austin.isd.org>
2021-07-27 15:03:39 -0700

    Create helloworldarm.py

---
+++ arm/helloworldarm.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# imports
+import rospy, sys, tf
+import moveit_commander
+from math import *
+from moveit_commander import MoveGroupCommander, PlanningSceneInterface
+
+GROUP_NAME_ARM = 'arm'
+
+class MoveItDemo:
+    def __init__(self):
+        rospy.loginfo("Set Arm: right_up")
+        arm.set_named_target('right_up')
+        if arm.go() != True:
+            rospy.logwarn("  Go failed")
+        rospy.sleep(2)
+
+if __name__ == "__main__":
+    MoveItDemo()
