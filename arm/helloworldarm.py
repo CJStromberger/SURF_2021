07181e4a48850988d72db41c9051211f83cc34ee
CJS <christian.stromberger65@stu.austin.isd.org>
2021-07-27 15:51:58 -0700

    Update helloworldarm.py

--- arm/helloworldarm.py
+++ arm/helloworldarm.py
@@ -4,13 +4,20 @@
 import rospy, sys, tf
 import moveit_commander
 from math import *
+from geometry_msgs.msg import PoseStamped
 from moveit_commander import MoveGroupCommander, PlanningSceneInterface
+from moveit_msgs.msg import PlanningScene, ObjectColor
+from moveit_msgs.msg import Grasp, GripperTranslation
+from moveit_msgs.msg import MoveItErrorCodes
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from tf.transformations import quaternion_from_euler
+from copy import deepcopy
 
 GROUP_NAME_ARM = 'arm'
 
 class MoveItDemo:
     def __init__(self):
-        
+
         arm = MoveGroupCommander(GROUP_NAME_ARM)
 
         rospy.loginfo("Set Arm: right_up")
