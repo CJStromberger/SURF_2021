cffea3e1de6289fba154498b23ea123a1f860ab4
CJS <christian.stromberger65@stu.austin.isd.org>
2021-11-18 23:35:24 -0800

    Fixed style in field.py

--- NEAT/field.py
+++ NEAT/field.py
@@ -1,98 +1,93 @@
+"""
+This class keeps track of the turtlebot's position and orientation, 
+as well as the goal's
+"""
+
 import math
 import constants
 class Field:
     # Turtle coords
-    turtx = 0 
-    turty = 0
-    turtrad = 0 # orientation of the turtlebot, 0 is pointing right
+    turt_x = 0 
+    turt_y = 0
+    turt_rad = 0 # orientation of the turtlebot, 0 is pointing right
     # Flag coords
-    flagx = 0
-    flagy = 0
+    flag_x = 0
+    flag_y = 0
 
     def __init__(self, turtle_x, turtle_y, turtle_rad, flag_x, flag_y):
-        self.turtx = turtle_x
-        self.turty = turtle_y
-        self.turtrad = turtle_rad
-        self.flagx = flag_x
-        self.flagy = flag_y
-        self.time = constants.TIME
+        self.turt_x = turtle_x
+        self.turt_y = turtle_y
+        self.turt_rad = turtle_rad
+        self.flag_x = flag_x
+        self.flag_y = flag_y
 
     # get turtle position and angle
-    def getTurtx(self):
-        return self.turtx
-    def getTurty(self):
-        return self.turty
-    def getTurtrad(self):
-        return self.turtrad
+    def get_turt_x(self):
+        return self.turt_x
+    def get_turt_y(self):
+        return self.turt_y
+    def get_turt_rad(self):
+        return self.turt_rad
 
     # get flag positions
-    def getFlagx(self):
-        return self.flagx
-    def getFlagy(self):
-        return self.flagy
-
-    # get time
-    def getTime(self):
-        return self.time
-    def setTime(self, new_time):
-        self.time = new_time
+    def get_flag_x(self):
+        return self.flag_x
+    def get_flag_y(self):
+        return self.flag_y
 
     # Flag position setting
     def setFlagPos(self, x, y):
-        self.flagx = x
-        self.flagy = y
+        self.flag_x = x
+        self.flag_y = y
 
     # Turtle position setting functions
-    def setTurt(self, x, y, rad):
-        self.turtx = x
-        self.turty = y
-        self.turtrad = rad
-    def setTurtPos(self, x, y):
-        self.turtx = x
-        self.turty = y
-    def setTurtAngle(self, rad):
-        self.turtrad = rad
+    def set_turt(self, x, y, rad):
+        self.turt_x = x
+        self.turt_y = y
+        self.turt_rad = rad
+    def set_turt_pos(self, x, y):
+        self.turt_x = x
+        self.turt_y = y
+    def set_turt_angle(self, rad):
+        self.turt_rad = rad
 
     # Pusing functions
     def push(self):
         # changning position
-        newx = self.turtx + constants.PUSH_DISTANCE_FORWARD*math.cos(self.turtrad) + constants.PUSH_DISTANCE_TANGENT*math.sin(self.turtrad)
-        newy = self.turty + constants.PUSH_DISTANCE_FORWARD*math.sin(self.turtrad) + constants.PUSH_DISTANCE_TANGENT*math.cos(self.turtrad)
+        new_x = self.turt_x + constants.PUSH_DISTANCE_FORWARD*math.cos(self.turt_rad) + constants.PUSH_DISTANCE_TANGENT*math.sin(self.turt_rad)
+        new_y = self.turt_y + constants.PUSH_DISTANCE_FORWARD*math.sin(self.turt_rad) + constants.PUSH_DISTANCE_TANGENT*math.cos(self.turt_rad)
 
         # changing angle
-        newrad = self.turtrad + constants.PUSH_RAD
+        new_rad = self.turt_rad + constants.PUSH_RAD
 
-        self.setTurtPos(newx,newy) #setting position
-        self.setTurtAngle(newrad) #setting radians
-        self.time = self.time - constants.PUSH_TIME #change the time
+        self.set_turt_pos(new_x,new_y) #setting position
+        self.set_turt_angle(new_rad) #setting radians
 
     # Pivoting clockwise
-    def pivotCW(self):
+    def pivot_cw(self):
         # turning pushes turtlebot forward
-        newx = self.turtx + constants.PIVOTCW_FORWARD*math.cos(self.turtrad) + constants.PIVOTCW_TANGENT*math.sin(self.turtrad)
-        newy = self.turty + constants.PIVOTCW_FORWARD*math.sin(self.turtrad) + constants.PIVOTCW_TANGENT*math.cos(self.turtrad)
+        new_x = self.turt_x + constants.PIVOTCW_FORWARD*math.cos(self.turt_rad) + constants.PIVOTCW_TANGENT*math.sin(self.turt_rad)
+        new_y = self.turt_y + constants.PIVOTCW_FORWARD*math.sin(self.turt_rad) + constants.PIVOTCW_TANGENT*math.cos(self.turt_rad)
 
         # changing angle
-        newrad = self.turtrad + constants.PIVOTCW_RAD
+        new_rad = self.turt_rad + constants.PIVOTCW_RAD
         
-        # update position, angle and time
-        self.setTurtPos(newx,newy)
-        self.setTurtAngle(newrad)
-        self.time = self.time - constants.PIVOT_TIME
+        # update position and angle
+        self.set_turt_pos(new_x,new_y)
+        self.set_turt_angle(new_rad)
 
     # Pivoting counterclockwise
-    def pivotCCW(self):
+    def pivot_ccw(self):
         # turning pushes turtlebot forward
-        newx = self.turtx + constants.PIVOTCCW_FORWARD*math.cos(self.turtrad) + constants.PIVOTCCW_TANGENT*math.sin(self.turtrad)
-        newy = self.turty + constants.PIVOTCCW_FORWARD*math.sin(self.turtrad) + constants.PIVOTCCW_TANGENT*math.cos(self.turtrad)
+        new_x = self.turt_x + constants.PIVOTCCW_FORWARD*math.cos(self.turt_rad) + constants.PIVOTCCW_TANGENT*math.sin(self.turt_rad)
+        new_y = self.turt_y + constants.PIVOTCCW_FORWARD*math.sin(self.turt_rad) + constants.PIVOTCCW_TANGENT*math.cos(self.turt_rad)
 
         # changing angle
-        newrad = self.turtrad + constants.PIVOTCCW_RAD
+        new_rad = self.turt_rad + constants.PIVOTCCW_RAD
         
-        # update position, angle and time
-        self.setTurtPos(newx,newy)
-        self.setTurtAngle(newrad)
-        self.time = self.time - constants.PIVOT_TIME
+        # update position and angle
+        self.set_turt_pos(new_x,new_y)
+        self.set_turt_angle(new_rad)
 
-    def distanceFromFlag(self):
-        return math.sqrt(pow(abs(self.turtx-self.flagx),2)+pow(abs(self.turty-self.flagy),2))
+    def distance_from_flag(self):
+        return math.sqrt(pow(abs(self.turt_x-self.flag_x),2)+pow(abs(self.turt_y-self.flag_y),2))
